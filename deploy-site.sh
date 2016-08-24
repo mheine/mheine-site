1d19c2623bb70cd4e9cf5f549362d9e9334ee6c4
Marcus Heine <mheine@kth.se>
2016-08-24 09:39:19 +0200

    Updates deploy script

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo "Tranferring files to mheine.se."
-rsync --recursive --progress css/ files/ fonts/ images/ js/ index.html mheine.se@ssh.mheine.se:/www/
+
+read -s -p "Type your SSH password for mheine.se: " SSHPASS
+eval "export SSHPASS='""$SSHPASS""'"
+
+echo ""
+
+sshpass -e rsync --recursive --progress css/ mheine.se@ssh.mheine.se:/www/css/ | tail -n +2
+sshpass -e rsync --recursive --progress files/ mheine.se@ssh.mheine.se:/www/files/ | tail -n +2
+sshpass -e rsync --recursive --progress fonts/ mheine.se@ssh.mheine.se:/www/fonts/ | tail -n +2
+sshpass -e rsync --recursive --progress images/ mheine.se@ssh.mheine.se:/www/images/ | tail -n +2
+sshpass -e rsync --recursive --progress js/ mheine.se@ssh.mheine.se:/www/js/ | tail -n +2
+sshpass -e rsync --recursive --progress index.html mheine.se@ssh.mheine.se:/www/ | tail -n +2
+
 echo "Transfer successful!"
