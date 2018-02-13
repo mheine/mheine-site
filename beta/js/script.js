ab7ce765b9e35837649bc6f9415331c852a3d755
Marcus Heine <mheine@kth.se>
2018-02-14 00:30:18 +0100

    Adds the basic beta structure

---
+++ beta/js/script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  mobileResize();
+};
+
+
+
+$( window ).resize(function() {
+	mobileResize();
+});
+
+
+function mobileResize() {
+	var $header = $("#header");
+
+	if (parseInt($header.css("font-size")) < 36) {
+		$header.css({ "font-size": "36px" });   
+	}
+
+	var $nav = $("#nav");
+
+	if (parseInt($(window).width()) < 330) 
+		$nav.css({ "width": "160px" });   
+
+	else 
+		$nav.css({ "width": "auto" });  
+}
