7d012aef8785771ae6c938eb5866129ffe581909
Marcus Heine <mheine@kth.se>
2018-02-19 20:35:18 +0100

    Fix favicon link and add thumbnail image

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-read -s -p "Type your SSH password for mheine.se: " SSHPASS
-eval "export SSHPASS='""$SSHPASS""'"
+ssh -q mheine.se@ssh.mheine.se exit
+STATUS=$?
 
-echo ""
+if [[ "$STATUS" != 0 ]] ; then
+	echo "SSH connection refused. Please check the ssh keys."
+	exit 0
+fi
 
-sshpass -e rsync --recursive --progress css/ mheine.se@ssh.mheine.se:/www/css/ | tail -n +2
-sshpass -e rsync --recursive --progress files/ mheine.se@ssh.mheine.se:/www/files/ | tail -n +2
-sshpass -e rsync --recursive --progress fonts/ mheine.se@ssh.mheine.se:/www/fonts/ | tail -n +2
-sshpass -e rsync --recursive --progress images/ mheine.se@ssh.mheine.se:/www/images/ | tail -n +2
-sshpass -e rsync --recursive --progress js/ mheine.se@ssh.mheine.se:/www/js/ | tail -n +2
-sshpass -e rsync --recursive --progress index.html mheine.se@ssh.mheine.se:/www/ | tail -n +2
+echo "Starting transfer to mheine.se"
+
+rsync --recursive --progress css/ mheine.se@ssh.mheine.se:/www/css/ | tail -n +2
+rsync --recursive --progress files/ mheine.se@ssh.mheine.se:/www/files/ | tail -n +2
+rsync --recursive --progress fonts/ mheine.se@ssh.mheine.se:/www/fonts/ | tail -n +2
+rsync --recursive --progress images/ mheine.se@ssh.mheine.se:/www/images/ | tail -n +2
+rsync --recursive --progress js/ mheine.se@ssh.mheine.se:/www/js/ | tail -n +2
+rsync --recursive --progress index.html mheine.se@ssh.mheine.se:/www/ | tail -n +2
+rsync --recursive --progress .htaccess mheine.se@ssh.mheine.se:/www/ | tail -n +2
 
 echo "Transfer successful!"
 
