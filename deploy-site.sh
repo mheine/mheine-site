dad1d8caedbddcaedc50994cfbfe956f1cca2c14
Marcus Heine <mheine@kth.se>
2018-03-21 18:15:46 +0100

    Add some pretty 404 and 403 pages

--- deploy-site.sh
+++ deploy-site.sh
@@ -16,6 +16,8 @@ rsync --recursive --progress fonts/ mheine.se@ssh.mheine.se:/www/fonts/ | tail -
 rsync --recursive --progress images/ mheine.se@ssh.mheine.se:/www/images/ | tail -n +2
 rsync --recursive --progress js/ mheine.se@ssh.mheine.se:/www/js/ | tail -n +2
 rsync --recursive --progress index.html mheine.se@ssh.mheine.se:/www/ | tail -n +2
+rsync --recursive --progress 404.html mheine.se@ssh.mheine.se:/www/ | tail -n +2
+rsync --recursive --progress 403.html mheine.se@ssh.mheine.se:/www/ | tail -n +2
 rsync --recursive --progress .htaccess mheine.se@ssh.mheine.se:/www/ | tail -n +2
 
 echo "Transfer successful!"
